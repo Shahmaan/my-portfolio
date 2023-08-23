599c025fdddc87f045710ca894fb05bab9841506
Nawaz Rahman <nawideus@gmail.com>
2023-08-23 17:14:18 +0200

    added style section to container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Who from "./components/Who";
 
 const Container = styled.div`
-height: 100vh;
-background-color: rebeccapurple;
+  height: 100vh;
+  background-color: rebeccapurple;
 `;
 
 function App() {
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { styled } from 'styled-components';
+
+const Section = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`;
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <Section>Projects</Section>
   )
 }
 
