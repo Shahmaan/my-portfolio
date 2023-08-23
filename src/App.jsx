ba381ad6bf9683ccfdbe78b5f694764cb45fdeb0
Nawaz Rahman <nawideus@gmail.com>
2023-08-23 20:00:42 +0200

    snap to grid

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,16 @@ import Who from "./components/Who";
 
 const Container = styled.div`
   height: 100vh;
-  background-color: rebeccapurple;
-`;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  color: white;
+  background: url("./img/bg.jpeg");
+  scrollbar-width: none;
+  &::-webkit-scrollbar{ 
+    display: none;
+  }
+  `;
 
 function App() {
   return (
