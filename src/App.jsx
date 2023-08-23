c198716ce40e4933975fa61fb9e9b5ea8ddd0506
Nawaz Rahman <nawideus@gmail.com>
2023-08-23 17:00:06 +0200

    backup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Who from "./components/Who";
+
+const Container = styled.div`
+height: 100vh;
+background-color: rebeccapurple;
+`;
+
+function App() {
+  return (
+    <Container>
+      <Hero />
+      <Who />
+      <Projects />
+      <Contact />
+    </Container>
+  );
+}
+
+export default App;
