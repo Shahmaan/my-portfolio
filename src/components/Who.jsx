96bf821c4f80f4a8b67244132d71c3027b03c8cb
Nawaz Rahman <nawideus@gmail.com>
2023-08-24 11:19:52 +0200

    started on 'who' page

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,15 +1,113 @@
-import React from 'react'
-import { styled } from 'styled-components';
+import React from "react";
+import { styled } from "styled-components";
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  
+`;
+const Title = styled.h1`
+  font-size: 70px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 30px;
+`;
+
+const Desc = styled.p`
+  color: lightgray;
+`;
+
+const TagLine = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 2px;
+  width: 15px;
+`;
+
+const Quote = styled.p`
+  color: #ff0000;
+`;
+
+const Button = styled.button`
+  background-color: #433751;
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+const Img = styled.img`
+  width: 800px;
+  height: 800px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+
+  @keyframes animate {
+    to {
+      transform: translateY(20px);
+    }
+  }
 `;
 
 const Who = () => {
   return (
-    <Section>Who</Section>
-  )
-}
+    <Section>
+      <Container>
+        <Left>{/* 3d model */}</Left>
+        <Right>
+          <Title>Nawaz A.Rahman</Title>
+          <Subtitle>Data Scientsit & Analyst</Subtitle>
+          <Desc>
+            Welcome to my corner of the data-driven universe! I'm a passionate
+            and curious data scientist on a mission to unravel the stories
+            hidden within the numbers. Armed with a deep love for
+            problem-solving and an insatiable curiosity, I embarked on this
+            exhilarating journey into the world of data science.
+          </Desc>
+          <TagLine>
+            <Line src="./img/line.png" />
+            <Quote>Where Data Meets Discovery</Quote>
+          </TagLine>
+          <Button>Learn More</Button>
+        </Right>
+      </Container>
+    </Section>
+  );
+};
 
-export default Who
+export default Who;
