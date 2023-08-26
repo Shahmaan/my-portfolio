27fca752b97fe0d088429b385d40863ccb551e8d
Nawaz Rahman <nawideus@gmail.com>
2023-08-26 12:45:59 +0200

    added content to 'Who' page

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -40,6 +40,7 @@ const Desc = styled.p`
   color: lightgray;
 `;
 
+
 const TagLine = styled.div`
   display: flex;
   align-items: center;
@@ -75,12 +76,49 @@ const Who = () => {
           <Title>Who Am I?</Title>
           {/*<Subtitle>Data Scientsit & Analyst</Subtitle>*/}
           <Desc>
-            Welcome to my corner of the data-driven universe! I'm a passionate
-            and curious data scientist on a mission to unravel the stories
-            hidden within the numbers. Armed with a deep love for
-            problem-solving and an insatiable curiosity, I embarked on this
-            exhilarating journey into the world of data science.
+            Greetings, fellow explorer! I'm Nawaz, a passionate Data Scientist
+            on a journey to decode the intricacies of data and turn them into
+            meaningful narratives. With a cup of curiosity and a spoonful of
+            creativity, I'm here to bridge the gap between complex datasets and
+            impactful insights.
           </Desc>
+          <Desc>
+            <b>The Explorer's Path</b>
+            <br />
+            As a recent graduate with a degree in Data Science, I've embarked on
+            a quest to navigate the ever-evolving landscape of data science. My
+            journey began with a fascination for patterns and an unrelenting
+            desire to understand the stories hidden within numbers.
+          </Desc>
+          <Desc>
+            <b>Unraveling the Data Tapestry</b>
+            <br />
+            From crafting predictive models that peer into the future, to
+            designing intuitive data visualizations that tell tales at a glance,
+            I've embraced the art of data science as both a passion and a
+            profession. With a toolkit that includes Python, SQL, R, Deep
+            Learning, Pandas, Numpy, and much more, I'm committed to uncovering
+            insights that drive smarter decisions.
+          </Desc>
+          <Desc>
+            <b>Beyond the Screen</b>
+            <br />
+            But data doesn't define me entirely. When I'm not immersed in the
+            world of algorithms and analytics, you might find me going on hikes,
+            reading, or playing sports. Because while data illuminates the path,
+            life's experiences add the colors that make the journey truly
+            vibrant.
+          </Desc>
+          <Desc>
+            <b>Join Me on this Odyssey</b>
+            <br />
+            Whether you're a fellow data enthusiast, a curious mind, or someone
+            seeking to transform data into actionable intelligence, I invite you
+            to join me on this odyssey. Let's embark together on a journey that
+            turns raw data into a symphony of insights, curiosity into clarity,
+            and numbers into narratives.
+          </Desc>
+
           <TagLine>
             <Line src="./img/line.png" />
             <Quote>Where Data Meets Discovery</Quote>
